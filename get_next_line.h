325298b5b696a7da55fd168dee22d7f70bab21a2
Mathis Francois <mafranco@car12s4.42barcelona.com>
2023-07-27 12:35:55 +0200

    esd

---
+++ get_next_line.h
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mafranco <mafranco@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/26 21:10:12 by mafranco          #+#    #+#             */
+/*   Updated: 2023/07/26 21:10:17 by mafranco         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <fcntl.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+
+char	*my_malloc(size_t nbytes, size_t size);
+
+char	*ft_substr(const char *total_buff, unsigned int start, size_t len);
+
+char	*ft_strjoin(char *total_buff, char *tmp);
+
+char	*fd_read(int fd, char *total_buff);
+
+char	*take_line(char *total_buff);
+
+char	*cut_static(char *total_buff);
+
+char	*get_next_line(int fd);
+
+char	*check_char(const char *str, int c);
+
+size_t	ft_strlen(const char *str);
+
+#endif
